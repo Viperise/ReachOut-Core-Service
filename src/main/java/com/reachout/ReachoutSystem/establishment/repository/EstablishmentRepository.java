f0d4e8dbe043c4821aaa61503cd347841e88f7ef
Rafael Soares <sawarafael@gmail.com>
2024-10-13 23:54:33 -0300

    feaT: add repositories for establishment context

---
+++ src/main/java/com/reachout/ReachoutSystem/establishment/repository/EstablishmentRepository.java
@@ -0,0 +1,7 @@
+package com.reachout.ReachoutSystem.establishment.repository;
+
+import com.reachout.ReachoutSystem.establishment.entity.Establishment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstablishmentRepository extends JpaRepository<Establishment, Long> {
+}
