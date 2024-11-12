587c2e91e70c70ed994241f520dd8336e39d3ca0
Rafael Soares <sawarafael@gmail.com>
2024-11-11 23:57:59 -0300

    fix: adjust smell codes on configurations

--- src/main/java/com/reachout/ReachoutSystem/configurations/FirebaseConfig.java
+++ src/main/java/com/reachout/ReachoutSystem/configurations/FirebaseConfig.java
@@ -2,17 +2,13 @@ package com.reachout.ReachoutSystem.configurations;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
