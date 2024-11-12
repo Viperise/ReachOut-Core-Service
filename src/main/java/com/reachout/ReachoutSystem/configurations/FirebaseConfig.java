bf13ea0b2f29c00416c813ed1902b3e9b11fd809
Rafael Soares <sawarafael@gmail.com>
2024-11-12 05:34:21 -0300

    fix: import modules from google credentials

--- src/main/java/com/reachout/ReachoutSystem/configurations/FirebaseConfig.java
+++ src/main/java/com/reachout/ReachoutSystem/configurations/FirebaseConfig.java
@@ -1,11 +1,10 @@
 package com.reachout.ReachoutSystem.configurations;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
-
+import com.google.auth.oauth2.GoogleCredentials;
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
